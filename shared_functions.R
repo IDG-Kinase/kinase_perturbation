7cb8817ddcafa4c0fd1c1a2ccc836e434311a048
Matthew Berginski <matthew.berginski@gmail.com>
2021-01-15 19:15:43 -0500

    cleaning up DESeq run to HGNC level and shifting functions into shared

--- shared_functions.R
+++ shared_functions.R
@@ -39,8 +39,56 @@ summarize_DESeq_results <- function(dds_results_filtered) {
 	))
 }
 
-get_full_DESeq_results <- function(drug_perturb_files, comparison_treatment = "DMSO") {
-	drug_perturb_files = drug_perturb_files %>%
+collect_full_DESeq_and_summary <- function(dds_analysis) {
+	full_DESeq = tribble(~hgnc_symbol,~ensembl_gene_id, ~baseMean, ~log2FoldChange, ~lfcSE, ~stat, ~pvalue, ~padj, ~treatment)
+	summary_stats = tribble(~treatment,~number_2fold,~num_kinases,~num_dark_kinases,~dk_diff_str)
+	
+	for (this_comparison in resultsNames(dds_analysis)[-1]) {
+		dds_results = lfcShrink(dds_analysis, coef=this_comparison, type="apeglm")
+		
+		full_DESeq = rbind(
+			full_DESeq,
+			dds_results %>%
+				as.data.frame() %>%
+				rownames_to_column(var = "hgnc_symbol") %>%
+				mutate(treatment = this_comparison) %>%
+				dplyr::select("hgnc_symbol","treatment",everything())
+		)
+		
+		dds_results_filtered = dds_results %>%
+			as.data.frame() %>%
+			rownames_to_column(var = "hgnc_symbol") %>%
+			filter(abs(log2FoldChange) >= 1, padj <= 0.05) %>%
+			mutate("treatment" = this_comparison)
+		
+		dds_results_filtered_kinase = dds_results_filtered %>%
+			filter(hgnc_symbol %in% all_kinases$symbol)
+		
+		dds_results_filtered_dark_kinase = dds_results_filtered %>%
+			filter(hgnc_symbol %in% dark_kinases$symbol)
+		
+		summary_stats = rbind(
+			summary_stats,
+			data.frame(
+				treatment = this_comparison,
+				number_2fold = dim(dds_results_filtered)[1],
+				num_kinases = dim(dds_results_filtered_kinase)[1],
+				num_dark_kinases = dim(dds_results_filtered_dark_kinase)[1],
+				dk_diff_str = ifelse(dim(dds_results_filtered_dark_kinase)[1] == 0,
+														 "",
+														 glue_collapse(sort(dds_results_filtered_dark_kinase$hgnc_symbol), sep=" | "))
+			)
+		)
+	}
+	
+	return(list(full = full_DESeq, summary = summary_stats))
+}
+
+get_DESeq_analysis <- function(drug_perturb_files, 
+															 comparison_treatment = "DMSO",
+															 DESeq_design = ~treatment) {
+	
+	drug_perturb_files = exp_info %>%
 		mutate(names = paste0(treatment,"_",batch,"_",rep)) %>%
 		#ensure that DMSO is the first level, forcing the fold change values to be
 		#calculated as drug_treatment/DMSO
@@ -52,21 +100,53 @@ get_full_DESeq_results <- function(drug_perturb_files, comparison_treatment = "D
 	drug_perturb_files$batch = as.factor(drug_perturb_files$batch)
 	drug_perturb_files$rep = as.factor(drug_perturb_files$rep)
 	
-	drug_perturb_exp = summarizeToGene(tximeta(drug_perturb_files))
-	dds <- DESeqDataSet(drug_perturb_exp, design = ~treatment)
+	transcript_to_hgnc = get_transcript_to_hgnc()
 	
-	#Code to deal with collapsing technical replicates into single columns in
-	#DESeq, will only be used if a "run" column is defined, which should be a
-	#unique name for each run, which collapses together in the names field.
-	if ("run" %in% colnames(drug_perturb_files)) {
-		dds <- collapseReplicates(dds, dds$names, dds$run)
-	}
+	mytx2gene = transcript_to_hgnc %>%
+		select(ensembl_transcript_id_version, hgnc_symbol)
+	
+	imported_tx <- tximport(exp_info$files, 
+													type = "salmon",
+													tx2gene = mytx2gene)
+	
+	dds <- DESeqDataSetFromTximport(imported_tx,
+																	colData = drug_perturb_files %>% select(-files),
+																	design = DESeq_design)
+	
+	#Toss out the rownames where all the ENSTs without HGNCs are grouped:
+	dds = dds[rownames(dds) != "",]
+	
 	keep <- rowSums(counts(dds)) > 1
 	dds <- dds[keep,]
 	
 	dds_analysis <- DESeq(dds)
-	dds_results = results(dds_analysis)
-	dds_results = lfcShrink(dds_analysis, coef=resultsNames(dds_analysis)[2], type="apeglm")
-	
-	return(dds_results)
 }
+
+
+get_transcript_to_hgnc <- function() {
+	if (! file.exists(here('transcript_to_hgnc.csv.gz'))) {
+		ensembl = biomaRt::useEnsembl("genes",dataset="hsapiens_gene_ensembl", version=95)
+		transcript_to_hgnc = biomaRt::getBM(attributes = c('ensembl_gene_id', 
+																											 'ensembl_transcript_id',
+																											 'ensembl_transcript_id_version',
+																											 'hgnc_symbol'), 
+																				mart = ensembl)
+		
+		transcript_to_hgnc = transcript_to_hgnc %>% 
+			#There are a small number of transcripts with multiple HGNC hits, I'm going to
+			#select only one
+			group_by(ensembl_transcript_id_version) %>% 
+			filter(row_number()==1) %>% 
+			ungroup()
+		
+		write_csv(transcript_to_hgnc, here('transcript_to_hgnc.csv.gz'))
+	} else {
+		transcript_to_hgnc = readr::read_csv(here('transcript_to_hgnc.csv.gz')) %>%
+			#The DESeq transcript to gene level summarization doesn't like NA values in
+			#the gene level targets, so I'm going to convert all the NA to "", so they
+			#can be grouped and tossed out later
+			mutate(hgnc_symbol = ifelse(is.na(hgnc_symbol), "", hgnc_symbol))
+			
+	}
+}
+

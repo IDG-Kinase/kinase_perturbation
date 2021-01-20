12183340f35490de0d25b9b27419475e326cc0e1
Matthew Berginski <matthew.berginski@gmail.com>
2021-01-20 14:37:05 -0500

    updating run all script with new data sets

--- raw_data/run_all_salmon.sh
+++ raw_data/run_all_salmon.sh
@@ -6,3 +6,7 @@ nice -n 19 ionice -c 3 ./run_salmon_Zawistowski.sh
 nice -n 19 ionice -c 3 ./run_salmon_HCC1806.sh
 nice -n 19 ionice -c 3 ./run_salmon_HCC1806_part2.sh
 nice -n 19 ionice -c 3 ./run_salmon_HCC1806_part3.sh
+
+nice -n 19 ionice -c 3 ./run_salmon_SUM229.sh
+nice -n 19 ionice -c 3 ./run_salmon_MDAMB231.sh
+nice -n 19 ionice -c 3 ./run_salmon_MDAMB468.sh
